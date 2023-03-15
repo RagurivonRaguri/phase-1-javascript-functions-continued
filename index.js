1b22feb4db90ab0aaed559bb3cee398b0f0dc343
LincolnMuraguri <lincolnmuraguri93@gmail.com>
2023-03-15 20:32:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(hobby='roller-skate') {
+        return `This Saturday, I want to ${hobby}!`;
+ }
+function mondayWork (work = 'go to the office') {
+        return `This Monday, I will ${work}.`
+}
+function wrapAdjective(art='*'){
+        return function(par='special') {
+                return `You are ${art}${par}${art}!`
+        }
+}
+wrapAdjective("||")("a dedicated programmer")
